51057c9da4ff2a36b3a236fa34af7998f6e2b82e
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2022-10-07 08:02:42 +0330

    fix ssl commands

--- ssl.sh
+++ ssl.sh
@@ -1,7 +1,7 @@
 HOST=$1
 PUBKEY=$2
 
-certbot --nginx  -d $HOST
+certbot -m aydin@djuno.io --agree-tos -n --nginx  -d $HOST
 
 sed -i "s~RELAY_PUBLIC_KEY=1~RELAY_PUBLIC_KEY=$PUBKEY~g" /var/djib/relay/.env
 sed -i "s~RELAY_DN=~RELAY_DN=$HOST~g" /var/djib/relay/.env
